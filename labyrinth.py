9f9859a349a06bf5ad0eace886bd6c65ba6352cf
Jonatan Westholm <jdw@sentian.ai>
2019-02-01 22:42:00 +0100

    no links

--- labyrinth.py
+++ labyrinth.py
@@ -1,5 +1,6 @@
 import random
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 import networkx as nx
 
 def grid_graph(n, m):
@@ -8,9 +9,9 @@ def grid_graph(n, m):
 		for j in range(m):
 			G.add_node((i, j))
 			if j < m - 1 and random.random() < 0.5:
-				G.add_edge((i, j), (i, j + 1), link = 1)
+				G.add_edge((i, j), (i, j + 1))
 			if i < n - 1 and random.random() < 0.5:
-				G.add_edge((i, j), (i + 1, j), link = 1)
+				G.add_edge((i, j), (i + 1, j))
 
 	return G
 
@@ -22,11 +23,16 @@ def draw_labyrinth(ax, L, n, m):
 
 	for i in range(n):
 		for j in range(m):
-			if (i, j + 1) in L[(i, j)] and L[(i, j)][(i, j + 1)]['link']:
+			if (i, j + 1) in L[(i, j)]:
 				ax.plot([j + 1, j + 1], [i, i + 1], 'k')
-			if (i + 1, j) in L[(i, j)] and L[(i, j)][(i + 1, j)]['link']:
+			if (i + 1, j) in L[(i, j)]:
 				ax.plot([j, j + 1], [i + 1, i + 1], 'k')
 
+	start_tile = Rectangle((0, 0), 1, 1, facecolor='g')
+	end_tile = Rectangle((n - 1, m - 1), 1, 1, facecolor='r')
+
+	ax.add_patch(start_tile)
+	ax.add_patch(end_tile)
 
 def main():
 	N = 10
