8284f8de1d3d691b2cf7d8fb039a7da895fff739
Jonatan Westholm <jdw@sentian.ai>
2019-02-03 19:45:41 +0100

    +dfs, add search tree to bfs and dfs

---
+++ common/search.py
@@ -0,0 +1,45 @@
+import networkx as nx
+from common.utils import Heap, shuffled
+
+def bfs(G, start, end):
+	T = nx.Graph() # the search tree
+	expanded_nodes = set()
+	heap = Heap()
+	heap.push((0, start))
+
+	while heap:
+		depth, node = heap.pop()
+		expanded_nodes.add(node)
+		if node == end:
+			break
+		for neigh in G[node]:
+			if neigh not in expanded_nodes:
+				T.add_edge(node, neigh)
+				heap.push((depth + 1, neigh))
+	else:
+		print("Warning: bfs did not find path from {} to {}".format(start, end))
+
+	dead_end_nodes = heap._h #set(T.nodes) - expanded_nodes
+	T.remove_nodes_from(dead_end_nodes)
+	return T
+
+def dfs(G, start, end):
+	T = nx.Graph() # the search tree
+	expanded_nodes = set()
+	stack = [(0, start)]
+
+	while stack:
+		depth, node = stack.pop()
+		expanded_nodes.add(node)
+		if node == end:
+			break
+		for neigh in shuffled(G[node]):
+			if neigh not in expanded_nodes:
+				T.add_edge(node, neigh)
+				stack.append((depth + 1, neigh))
+	else:
+		print("Warning: dfs did not find path from {} to {}".format(start, end))
+
+	dead_end_nodes = set(T.nodes) - expanded_nodes
+	T.remove_nodes_from(dead_end_nodes)
+	return T
