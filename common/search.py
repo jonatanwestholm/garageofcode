3328ee55b52c282e5b57a7ae567a6463318a85fe
Jonatan Westholm <jdw@sentian.ai>
2019-02-03 20:41:11 +0100

    fixing bug in bfs

--- common/search.py
+++ common/search.py
@@ -9,18 +9,25 @@ def bfs(G, start, end):
 
 	while heap:
 		depth, node = heap.pop()
+		#print(node)
 		expanded_nodes.add(node)
 		if node == end:
 			break
 		for neigh in G[node]:
 			if neigh not in expanded_nodes:
+				#print("\t", neigh)
 				T.add_edge(node, neigh)
 				heap.push((depth + 1, neigh))
+			else:
+				pass#print("\t", neigh, "v")
 	else:
 		print("Warning: bfs did not find path from {} to {}".format(start, end))
 
-	dead_end_nodes = heap._h #set(T.nodes) - expanded_nodes
-	T.remove_nodes_from(dead_end_nodes)
+	#dead_end_nodes = heap._h #set(T.nodes) - expanded_nodes
+	#T.remove_nodes_from(dead_end_nodes)
+	#print("Start in dead_end_nodes:", start in T)
+	#print("End in dead_end_nodes:", end in T)
+	#print("END\n\n")
 	return T
 
 def dfs(G, start, end):
