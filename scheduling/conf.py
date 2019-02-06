553445937e086a7bce5ccf1097f5e9ee670d49f0
Jonatan Westholm <jonatanwestholm@gmail.com>
2019-02-07 00:46:40 +0100

    simple read from test data format

---
+++ scheduling/conf.py
@@ -0,0 +1,19 @@
+
+fn = "/home/jdw/garageofcode/results/private_tutoring_scheduling/test_data.csv"
+
+date_format = "%Y-%m-%d"
+time_format = "%H:%M"
+
+# Set params and generate random data
+D = 5 # num days
+T_D = 10 # num times per day
+T = D * T_D
+N = 10 # num students
+max_simultaneous = [3 for _ in range(T)] # max num student per time
+min_times = [1 for _ in range(N)] # min times per student
+
+#available = [[random.random() < 0.1 for _ in range(T)] for _ in range(N)]
+
+# Preference parameters
+per_diem_cost = 200
+time_cost = 30
---
+++ scheduling/read.py
@@ -0,0 +1,26 @@
+import csv
+from collections import defaultdict
+from datetime import datetime
+
+import scheduling.conf as conf
+
+def read(fn):
+	dt_format = conf.date_format + conf.time_format
+	students = set()
+	times = set()
+	available = defaultdict(set)
+
+	with open(fn, "r") as f:
+		reader = csv.DictReader(f, delimiter=',')
+		#reader.next() # skip header
+		for row in reader:
+			if not row["student_id"]:
+				continue
+			student = row["student_id"]
+			dt = datetime.strptime(row["date"] + row["time"], dt_format)
+
+			students.add(student)
+			times.add(dt)
+			available[student].add(dt)
+
+	return students, times, available
