ba91a307633544826f32c42323e73a2accdc4869
Jonatan Westholm <jonatanwestholm@gmail.com>
2019-02-14 01:10:59 +0100

    working langford pairs solver

--- sat/main.py
+++ sat/main.py
@@ -2,27 +2,84 @@ import sys
 import os
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
+from itertools import zip_longest
+from collections import defaultdict
+
+from common.utils import flatten_simple
 from sat.solver import SugarRush
+
+def langford(solver, n):
+    X = [[solver.var() for _ in range(2*n - k - 2)] for k in range(n)]
+
+    for row in X:
+        solver.symmetric(row)
+
+    position2covering = defaultdict(list)
+    for k, row in enumerate(X):
+        for i, var in enumerate(row):
+            position2covering[i].append(var)
+            position2covering[i + k + 2].append(var)
+
+    for lits in position2covering.values():
+        solver.symmetric(lits)
+
+    return X
+
+def print_langford_solution(solver, X):
+    position2num = {}
+    for k, row in enumerate(X):
+        row_solve = [solver.solution_value(var) for var in row]
+        #print(row_solve)
+        idx0 = row_solve.index(True)
+        idx1 = idx0 + k + 2
+
+        if idx0 in position2num:            
+            print("{} already taken by {}".format(idx0, position2num[idx0]))
+        else:
+            position2num[idx0] = k + 1
+
+        if idx1 in position2num:            
+            print("{} already taken by {}".format(idx1, position2num[idx1]))
+        else:
+            position2num[idx1] = k + 1
+
+    _, nums = zip(*sorted(position2num.items()))
+    langford_str = ",".join(map(str, nums))
+    print(langford_str)
 
 def main():
     solver = SugarRush()
 
-    a = solver.var()
-    b = solver.var()
-    d = solver.var()
-    c = solver.var()
+    X = langford(solver, 12)
 
-    cnf = [[a, -b, -c], [-a, b, -c], [-a, -b, c]]
+    satisfiable = solver.solve()
+    print(satisfiable)
+    if not satisfiable:
+        return
+
+    print_langford_solution(solver, X)
+
+    '''
+    X = [solver.var() for _ in range(20)]
+
+    for enctype in [EncType.pairwise, EncType.seqcounter, EncType.bitwise]: 
+        cnf = CardEnc.atmost(lits=X, encoding=enctype).clauses
+        print(cnf)
+        print()
+
+    return
+
+    print(cnf)
 
     solver.add_clauses_from(cnf)
 
     solver.solve()
     #print(solver.get_model())
     #>> [-1, -2]
+    '''
 
-    for var in [a, b, c, d]:
-        print("Var: {}, Value: {}".format(var, solver.solution_value(var)))
-    solver.solution_value(d)
+    #for var in flatten_simple(X):
+    #    print("Var: {}, Value: {}".format(var, solver.solution_value(var)))
     #print(solver.get_model())
 
 if __name__ == '__main__':
--- sat/solver.py
+++ sat/solver.py
@@ -1,14 +1,26 @@
+from common.utils import flatten_simple
+
 from pysat.solvers import Solver
+from pysat.card import CardEnc, EncType
 
 class SugarRush(Solver):
+    """
+    Quality-of-life wrapper for pysat.solvers.Solver
+    """
     def __init__(self, name="glucose4"):
         super().__init__(name=name)
-        self.var_num = 0
         self.var2val = {}
-        
+
+    """
+    Basics
+    """
     def var(self):
-        self.var_num += 1
-        return self.var_num
+        if self.nof_vars() == -1:
+            id_num = 1
+        else:
+            id_num = self.nof_vars() + 1
+        self.add_clause([id_num, -id_num])
+        return id_num
 
     def add_clauses_from(self, cnf):
         return self.append_formula(cnf)        
@@ -21,3 +33,12 @@ class SugarRush(Solver):
         if not self.var2val:
             self._init_var2val()
         return self.var2val[var]
+
+    """
+    Constructs
+    """
+    def symmetric(self, lits, encoding=EncType.seqcounter):
+        cnf = CardEnc.equals(lits=lits, 
+                              encoding=encoding, 
+                              top_id=self.nof_vars())
+        self.add_clauses_from(cnf.clauses)
