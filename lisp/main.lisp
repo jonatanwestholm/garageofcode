f26bccf70e22a28db18f249239ca856f15e0d8c1
Jonatan Westholm <jonatanwestholm@gmail.com>
2019-09-11 23:34:53 +0200

    working on in-program implementation of defun

--- lisp/main.lisp
+++ lisp/main.lisp
@@ -65,30 +65,49 @@
                      (evlis. (cdr m) a)))))
 
 (defun eval. (e a)
-(cond
-    ((atom e) (assoc. e a))
-    ((atom (car e))
-        (cond
-        ((eq (car e) 'quote) (cadr e))
-        ((eq (car e) 'atom) (atom (eval. (cadr e) a)))
-        ((eq (car e) 'eq) (eq (eval. (cadr e) a)
-                              (eval. (caddr e) a)))
-        ((eq (car e) 'car) (car (eval. (cadr e) a)))
-        ((eq (car e) 'cdr) (cdr (eval. (cadr e) a)))
-        ((eq (car e) 'cons) (cons (eval. (cadr e) a)
+    (cond
+        ((atom e) (assoc. e a))
+        ((atom (car e))
+            (cond
+            ((eq (car e) 'quote) (cadr e))
+            ((eq (car e) 'atom) (atom (eval. (cadr e) a)))
+            ((eq (car e) 'eq) (eq (eval. (cadr e) a)
                                   (eval. (caddr e) a)))
-        ((eq (car e) 'cond) (evcon. (cdr e) a))
-        ('t (eval. (cons (assoc. (car e) a)
-        (cdr e))
-        a))))
-    ((eq (caar e) 'label)
-        (eval. (cons (caddar e) (cdr e))
-               (cons (list (cadar e) (car e)) a)))
-    ((eq (caar e) 'lambda)
-        (eval. (caddar e)
-               (append. (pair. (cadar e) (evlis. (cdr e) a))
-                        a)))))
+            ((eq (car e) 'car) (car (eval. (cadr e) a)))
+            ((eq (car e) 'cdr) (cdr (eval. (cadr e) a)))
+            ((eq (car e) 'cons) (cons (eval. (cadr e) a)
+                                      (eval. (caddr e) a)))
+            ((eq (car e) 'cond) (evcon. (cdr e) a))
+            ('t (eval. (cons (assoc. (car e) a)
+                             (cdr e))
+            a))))
+        ((eq (caar e) 'label.)
+            (eval. (cons (caddar e) (cdr e))
+                   (cons (list (cadar e) (car e)) a)))
+        ((eq (caar e) 'lambda.)
+            (eval. (caddar e)
+                   (append. (pair. (cadar e) (evlis. (cdr e) a))
+                            a)))))
 
+;(defun defun. (f args exp))
+
+; ok, challenge: define "defun." without using defun, only eval.
+; I want to be able to write:
+;(defun. f (x) (+ x 1))
+;(write (f 1)) ;should return 2
+
+(eval. 
+    ((label defun. (lambda. (fname args expr) ()))
+     ((defun. f (x) (+ x 1))
+      (write (f 1)))
+    ) 
+    '())
+
+
+
+#|
 (write 
     (eval. 'a 
            '((a x) (b y))))
+|#
+
