84b575acf14595eb1a2fba57081617d18c649aef
jonatanwestholm <jonatanwestholm@gmail.com>
2019-10-05 23:35:13 +0200

    calculate width for printing

--- garageofcode/common/benchmarking.py
+++ garageofcode/common/benchmarking.py
@@ -5,36 +5,37 @@ from collections import defaultdict
 def run(funcs, params):
     p_docs = [p_doc for p_doc, _ in params.items()]
     f_docs = [f_doc for f_doc, _ in funcs.items()]
+    width = max(map(len, p_docs))
 
-    header_format = " " * 15 + "{:>15s}" * len(f_docs) * 2 + "{:>15}".format("all equal")
+    header_format = " " * width + "".join(["{" + ":>{}s".format(width) + "}"] * len(f_docs)) * 2
+    header_format = header_format + "{0:>{1}}".format("all equal", width)
     print(header_format.format(*f_docs, *f_docs))
     #row_format = "{:<15s}" + "{:>15}" * len(f_docs) + "{:>15}" * len(f_docs) + "{:>15}"
 
     res = {}
     for p_doc, p in params.items():
         res[p_doc] = {f_doc: ("", "") for f_doc in funcs}
-        print_row(p_doc, res[p_doc])
+        print_row(p_doc, res[p_doc], width)
         for f_doc, func in funcs.items():
             t0 = time.time()
             r = func(*p)
             t1 = time.time()
             res[p_doc][f_doc] = (t1-t0, r)
-            print_row(p_doc, res[p_doc])
+            print_row(p_doc, res[p_doc], width)
         print()
 
 
-def print_row(p_doc, f2res):
-    # TODO: calculate appropriate width instead of hard code
+def print_row(p_doc, f2res, width):
     # TODO: calculate appropriate accuracy in milliseconds
     row_format = ""
     for _, (t, r) in f2res.items():
         if type(t) == type(""):
-            row_format = row_format + "{:>15s}"
+            row_format = row_format + "{" + ":>{}s".format(width) + "}"
         else:
-            row_format = row_format + "{:>15.1f}"
+            row_format = row_format + "{" + ":>{}.1f".format(width) + "}"
     row_format = row_format * 2
-    row_format = "{:<15s}" + row_format
-    row_format = row_format + "{:>15s}"
+    row_format = "{" + ":<{}s".format(width) + "}" + row_format
+    row_format = row_format + "{" + ":>{}s".format(width) + "}"
 
     f2t, f2r = zip(*[(1000*t, r) for f_doc, (t, r) in f2res.items()])
     all_equal = "ok" if all([f2r[0] == r for r in f2r]) else "not"
