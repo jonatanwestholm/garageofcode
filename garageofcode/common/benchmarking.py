04fd94d89a5933fa4a6360a3763eb1b0394e2f55
jonatanwestholm <jonatanwestholm@gmail.com>
2019-10-10 21:37:03 +0200

    accuracy parameter in benchmarking. benching script for descendants and ancesters

--- garageofcode/common/benchmarking.py
+++ garageofcode/common/benchmarking.py
@@ -2,7 +2,7 @@ import time
 import pprint
 from collections import defaultdict
 
-def run(funcs, params):
+def run(funcs, params, **kwargs):
     p_docs = [p_doc for p_doc, _ in params.items()]
     f_docs = [f_doc for f_doc, _ in funcs.items()]
     width = max(map(len, p_docs))
@@ -15,24 +15,24 @@ def run(funcs, params):
     res = {}
     for p_doc, p in params.items():
         res[p_doc] = {f_doc: ("", "") for f_doc in funcs}
-        print_row(p_doc, res[p_doc], width)
+        print_row(p_doc, res[p_doc], width, **kwargs)
         for f_doc, func in funcs.items():
             t0 = time.time()
             r = func(*p)
             t1 = time.time()
             res[p_doc][f_doc] = (t1-t0, r)
-            print_row(p_doc, res[p_doc], width)
+            print_row(p_doc, res[p_doc], width, **kwargs)
         print()
 
 
-def print_row(p_doc, f2res, width):
+def print_row(p_doc, f2res, width, decimals=1):
     # TODO: calculate appropriate accuracy in milliseconds
     row_format = ""
     for _, (t, r) in f2res.items():
         if type(t) == type(""):
-            row_format = row_format + "{" + ":>{}s".format(width) + "}"
+            row_format = row_format + "{" + ":>{0}s".format(width) + "}"
         else:
-            row_format = row_format + "{" + ":>{}.1f".format(width) + "}"
+            row_format = row_format + "{" + ":>{0}.{1}f".format(width, decimals) + "}"
     row_format = row_format * 2
     row_format = "{" + ":<{}s".format(width) + "}" + row_format
     row_format = row_format + "{" + ":>{}s".format(width) + "}"
