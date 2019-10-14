ec098001b131c2dcdb6a0a9e2c7c1ea0833df7d0
jonatanwestholm <jonatanwestholm@gmail.com>
2019-10-14 11:50:33 +0200

    optimized representation

--- garageofcode/compression/lz78.py
+++ garageofcode/compression/lz78.py
@@ -2,7 +2,7 @@ import os
 from collections import defaultdict
 #import networkx as nx
 
-splitter = "¤"  # issue: crash if 'splitter' is in text
+splitter = " "
 
 def encode(reader):
     G = defaultdict(dict)  # phrase tree
@@ -23,14 +23,28 @@ def encode(reader):
 def decode(reader):
     S = {}  # child -> (parent, symbol) mapping in phrase tree
     top = 0
-    for line in reader:
-        seq, a = line.split(splitter)
-        seq = int(seq)
-        a = a[:-1]  # remove newline
+    reader = iter(reader.read())
+    while True:
+        try:
+            seq = get_id(reader)
+        except StopIteration:
+            break
+        a = next(reader)
+        #print("a", a)
         yield from climb_to_root(S, seq)
         yield a
         top += 1
         S[top] = (seq, a)
+
+def get_id(reader):
+    id_num = []
+    while True:
+        a = next(reader)
+        #print("digit", a)
+        if a not in "0123456789":
+            return int("".join(id_num))
+        else:
+            id_num.append(a)
 
 def climb_to_root(S, seq):
     """
@@ -47,18 +61,18 @@ def climb_to_root(S, seq):
         yield a
 
 def main():
-    #fn = "/home/jdw/garageofcode/data/compression/nilsholg.txt"
-    fn = "veryshort.txt"
+    fn = "/home/jdw/garageofcode/data/compression/nilsholg.txt"
+    #fn = "short.txt"
     fn_compressed = fn.split(".")[0] + ".wzip"
     fn_reconstructed = fn.split(".")[0] + "_rec.txt"
     # encoding step
     with open(fn, "r") as r:
         with open(fn_compressed, "w") as f:
             for seq, a in encode(r):
-                if a == "\n":
-                    f.write("{}{}\\n\n".format(seq, splitter))
-                else:
-                    f.write("{}{}{}\n".format(seq, splitter, a))
+                #if a == "\n":
+                #    f.write("{}{}\\n".format(seq, splitter))
+                #else:
+                f.write("{}{}{}".format(seq, splitter, a))
     print("Before ", os.stat(fn).st_size)
     print("After  ", os.stat(fn_compressed).st_size)
 
