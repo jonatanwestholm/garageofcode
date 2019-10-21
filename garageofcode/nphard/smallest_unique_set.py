466831bf6a12286fc2137f7d2ea7f5ce42d29ea5
jonatanwestholm <jonatanwestholm@gmail.com>
2019-10-21 22:58:34 +0200

    comparing greedy with naive

--- garageofcode/nphard/smallest_unique_set.py
+++ garageofcode/nphard/smallest_unique_set.py
@@ -19,25 +19,54 @@ def powerset(s):
 
 
 def smallest_unique_set(S, subset=None):
-    """Naive version"""
+    """Greedy version"""
 
     if subset is None:
         subset = set(chain.from_iterable(S))
-    for si in S:
-        if not subset - set(si):
-            raise ValueError("No unique set exists")
     p = []
     while S:
         c = Counter(chain.from_iterable(S))
+        c = Counter({k: v for k, v in c.items() if k in subset})
         if subset - set(c):
             r = next(iter(subset - set(c)))
             p.append(r)
             break        
         else:
-            r = c.most_common()[-1][0]
+            r, freq = c.most_common()[-1]
+            if freq >= len(S):
+                raise ValueError("No unique set exists")
             p.append(r)
             S = [si for si in S if r in si]
+            print("Remaining elements:", len(S))
     return p
+
+
+def smallest_unique_set_naive(S, subset=None):
+    """Naive version"""
+
+    if subset is None:
+        subset = set(chain.from_iterable(S))
+    p = []
+    while S:
+        c = subset & set(chain.from_iterable(S))
+        if subset - c:
+            r = next(iter(subset - c))
+            p.append(r)
+            break        
+        else:
+            c = iter(c)
+            while True:
+                try:
+                    r = next(c)
+                except StopIteration:
+                    raise ValueError("No unique set exists")
+                if r not in p:
+                    p.append(r)
+                    break
+            S = [si for si in S if r in si]
+            print("Remaining elements:", len(S))
+    return p
+
 
 
 def smallest_unique_set_exhaustive(S, subset=None):
@@ -60,13 +89,25 @@ def smallest_unique_set_exhaustive(S, subset=None):
 
 def main():
     #S = [[1, 2], [1, 2, 3], [1, 3], [2, 3], [3, 4]]
-    S = [si for si in get_data(100000, 500, 10000)]
+    n = 10000  # number of documents
+    k = 500  # document length
+    b = 1000  # words in dict
+    S = [si for si in get_data(n, k, b)]
+    S, subset = S[:-1], S[-1]
+    #print(subset)
+    #subset = set(range(b))
     t0 = time.time()
-    naive = smallest_unique_set(S)
+    naive = smallest_unique_set_naive(S, subset)
     t1 = time.time()
     print("Naive:", naive)
     print("Naive len:", len(naive))
     print("Naive time: {0:.3f}".format(t1 - t0))
+    t0 = time.time()
+    naive = smallest_unique_set(S, subset)
+    t1 = time.time()
+    print("Greedy:", naive)
+    print("Greedy len:", len(naive))
+    print("Greedy time: {0:.3f}".format(t1 - t0))
     '''
     t0 = time.time()
     exhaustive = smallest_unique_set_exhaustive(S)
