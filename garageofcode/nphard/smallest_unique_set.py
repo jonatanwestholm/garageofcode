161beeb28227be39e54e564e1c09016eadc9c4b2
jonatanwestholm <jonatanwestholm@gmail.com>
2019-10-21 22:18:37 +0200

    comparing with exhaustive

--- garageofcode/nphard/smallest_unique_set.py
+++ garageofcode/nphard/smallest_unique_set.py
@@ -1,20 +1,35 @@
+import time
+from itertools import chain, combinations
 from collections import Counter
 
-def elems(S):
-    for si in S:
-        yield from si
+import numpy as np
+
+def get_data(n, k, b):
+    """returns n random sets of length k
+    with elements from 0..b-1
+    """
+
+    for _ in range(n):
+        yield set(np.random.randint(0, b, size=k))
+
+
+def powerset(s):
+    s = list(s)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
 
 def smallest_unique_set(S, subset=None):
+    """Naive version"""
+
     if subset is None:
-        subset = set(elems(S))
+        subset = set(chain.from_iterable(S))
     for si in S:
         if not subset - set(si):
             raise ValueError("No unique set exists")
     p = []
     while S:
-        c = Counter(elems(S))
-        if len(c) < len(subset):  
-            # it will break after this
+        c = Counter(chain.from_iterable(S))
+        if subset - set(c):
             r = next(iter(subset - set(c)))
             p.append(r)
             break        
@@ -25,10 +40,45 @@ def smallest_unique_set(S, subset=None):
     return p
 
 
+def smallest_unique_set_exhaustive(S, subset=None):
+    """Exhaustive version"""
+
+    if subset is None:
+        subset = set(chain.from_iterable(S))
+    for si in S:
+        if not subset - set(si):
+            raise ValueError("No unique set exists")
+
+    for s in powerset(subset):
+        s = set(s)
+        for si in S:
+            if not s - si:
+                break
+        else:
+            return s
+
+
 def main():
-    S = [[1, 2], [1, 2, 3], [1, 3], [2, 3], [3, 4]]
-    print(smallest_unique_set(S))
+    #S = [[1, 2], [1, 2, 3], [1, 3], [2, 3], [3, 4]]
+    S = [si for si in get_data(100000, 500, 10000)]
+    t0 = time.time()
+    naive = smallest_unique_set(S)
+    t1 = time.time()
+    print("Naive:", naive)
+    print("Naive len:", len(naive))
+    print("Naive time: {0:.3f}".format(t1 - t0))
+    '''
+    t0 = time.time()
+    exhaustive = smallest_unique_set_exhaustive(S)
+    t1 = time.time()
+    print("Exhaustive:", exhaustive)
+    print("Exhaustive len:", len(exhaustive))
+    print("Exhaustive time: {0:.3f}".format(t1 - t0))
+    '''
 
 
 if __name__ == '__main__':
     main()
+
+    #for elem in get_data(100, 5, 7):
+    #    print(elem)
