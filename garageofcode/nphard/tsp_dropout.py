e57a1a01ad58c52f828e37d8b72c52e0fd927a42
jonatanwestholm <jonatanwestholm@gmail.com>
2019-10-25 15:18:57 +0200

    initial version of ruin and recreate

--- garageofcode/nphard/tsp_dropout.py
+++ garageofcode/nphard/tsp_dropout.py
@@ -11,6 +11,12 @@ from garageofcode.mip.tsp import tsp as tsp_mip
 
 class TSPath:
     def __init__(self, points=None, G=None):
+        """Should I restrict this to having input either
+        points or G? What are the use cases of having both?
+        Spinning off sub-optimizations with an init path?
+        YAGNI, probably
+        It's just so ugly now
+        """
         if points is not None:
             self.N = len(points)
             self.points = points
@@ -21,9 +27,12 @@ class TSPath:
             self.G = {}  # single linked list
             for i in range(self.N):
                 self.G[i] = (i+1) % self.N
+            self.cycle_nodes = set(range(self.N))
         else:
             self.N = len(G)
             self.G = G
+            self.cycle_nodes = set(G)
+        self.in_cycle = self.N
 
     def greedy_init(self):
         node = next(iter(self.G))
@@ -34,7 +43,8 @@ class TSPath:
             path.append(nearest)
             remaining.remove(nearest)
             node = nearest
-        return path
+        for i, j in zip(path, path[1:] + [path[0]]):
+            self.G[i] = j
 
     def get_path(self, i0=0):
         i = i0
@@ -73,7 +83,7 @@ class TSPath:
     def triple_swap(self, u0, u1, u2):
         G = self.G
         G[u0], G[u1], G[u2] = G[u2], G[u0], G[u1]
-        if self.get_pathlen() < self.N:
+        if self.get_pathlen() < self.in_cycle:
             # in case orientation was wrong
             # it will only have to recurse once
             self.triple_swap(u0, u1, u2)
@@ -84,7 +94,7 @@ class TSPath:
         u0 = self.reverse_cycle(u0)
         #print("u0:", u0)
         G[u0], G[u1] = G[u1], G[u0]  # ha-ha!
-        if self.get_pathlen() < self.N:
+        if self.get_pathlen() < self.in_cycle:
             raise RuntimeError("dropped nodes!")
 
     def improving_cross(self, u0, u1):
@@ -92,6 +102,67 @@ class TSPath:
         D = self.D
         v0, v1 = G[u0], G[u1]
         return D[u0, u1] + D[v0, v1] < D[u0, v0] + D[u1, v1]
+
+    def remove_head(self, u):
+        """
+        Removes the head of u from the path
+        Returns the removed node
+        """
+        G = self.G
+        v = G[u]
+        if u == v:
+            # u is already single
+            # don't decrement in_cycle
+            return u
+        G[u], G[v] = G[v], G[u]
+        self.in_cycle -= 1
+        self.cycle_nodes.remove(v)
+        return v
+
+    def insert(self, u, v):
+        """
+        Inserts v at the head of u
+        Assumes that G[v] = v
+        """
+        G = self.G
+        if u == v:
+            # don't increment in_cycle
+            return
+        G[u], G[v] = G[v], G[u]
+        self.in_cycle += 1
+        self.cycle_nodes.add(v)
+
+    def detour(self, u0, u1, v):
+        """
+        What is the cost difference of 
+        doing a detour through v when 
+        going from u0 to u1?
+        """
+        D = self.D
+        return D[u0, v] + D[v, u1] - D[u0, u1]
+
+    def optimal_insert(self, v):
+        """
+        Which u gives the lowest cost increase
+        with self.insert(u, v)?
+        """
+        return min(self.cycle_nodes, key=lambda u: self.detour(u, self.G[u], v))
+
+    def ruin(self, nodes):
+        """
+        Remove the heads of `nodes`
+        Return the removed nodes
+        """
+        # should be a set in case v = G[u], for some u, v both in nodes
+        return set([self.remove_head(u) for u in nodes])
+
+    def recreate(self, nodes):
+        """
+        Simple, incremental recreate step
+        """
+        for v in nodes:
+            self.insert(self.optimal_insert(v), v)
+
 
 
 def get_data(n, r):
@@ -112,7 +183,10 @@ def test_cross_swap():
     tspath.cross_swap(0, 2)
     print("G1:", tspath.G)
 
-def tsp(points):
+def tsp_local(points):
+    """
+    Random greedy search with local operations
+    """
     N = len(points)
     tspath = TSPath(points)
     tspath.greedy_init()
@@ -133,11 +207,42 @@ def tsp(points):
             if new_score <= score:
                 score = new_score
             else:
+                # metropolis-hastings
                 #if np.random.rand() > 0: #10**((score - new_score) / 10 * np.log(i+1)):
                 tspath.triple_swap(*(reversed(u)))
-            
-            #else:
-            #    score = new_score
+
+    return tspath.get_path()
+
+def tsp_ruin_recreate(points):
+    R = 20  # radius for ruin
+
+    N = len(points)
+    tspath = TSPath(points)
+    tspath.greedy_init()
+
+    score = tspath.get_score()
+    for i in range(100000):
+        if i % 10000 == 0:
+            print("{0:.1f}".format(score))
+        # ruin step
+        u = np.random.randint(N)
+        nodes = filter(lambda v: tspath.D[v, u] < R, tspath.G)
+        #nodes = [u]
+
+        #v = tspath.remove_head(u)
+        #tspath.insert(u, v)
+        singles = tspath.ruin(nodes)
+
+        # recreate step
+        tspath.recreate(singles)
+        '''
+        '''
+        if tspath.get_pathlen() < N:
+            print("i:", i)
+            print("u:", u)
+            print("G:", tspath.G)
+            raise RuntimeError("dropped nodes!")
+        score = tspath.get_score()
 
     return tspath.get_path()
 
@@ -145,12 +250,13 @@ def tsp(points):
 def main():
     np.random.seed(0)
     #  problem parameters
-    n = 10
+    n = 100
     k = 4
     r = 100
 
     #  solution parameters
     num_iter = 20
+    tsp = tsp_ruin_recreate
 
     points = get_data(n, r)
     t0 = time.time()
