2f43e8c4a7ea57c0f9c525c1f7edd13f0537cca1
jonatanwestholm <jonatanwestholm@gmail.com>
2019-10-25 11:27:07 +0200

    add cross swap, better results with init

--- garageofcode/nphard/tsp_dropout.py
+++ garageofcode/nphard/tsp_dropout.py
@@ -16,13 +16,13 @@ def get_data(n, r):
     return np.random.random([n, 2]) * r
 
 
-def get_path(G):
-    i = 0
+def get_path(G, i0=0):
+    i = i0
     path = []
     while True:
         path.append(i)
         i = G[i]
-        if not i:
+        if i == i0:
             return path
 
 
@@ -43,14 +43,44 @@ def get_score(G, D):
         score += D[i, j]
     return score
 
+def reverse_cycle(G, u0):
+    path = get_path(G, u0)
+    for i, j in zip(path, [path[-1]] + path[:-1]):
+        G[i] = j
+    if len(path) > 1:
+        return path[1]
+    else:
+        return path[0]
 
-def swap(G, u0, u1, u2):
+def triple_swap(G, u0, u1, u2):
     G[u0], G[u1], G[u2] = G[u2], G[u0], G[u1]
-    if get_pathlen(G) < N:
-        # in case, orientation was wrong
+    if get_pathlen(G) < len(G):
+        # in case orientation was wrong
         # it will only have to recurse once
-        swap(G, u0, u1, u2)
+        triple_swap(G, u0, u1, u2)
 
+def cross_swap(G, u0, u1):
+    G[u0], G[u1] = G[u1], G[u0]
+    u0 = reverse_cycle(G, u0)
+    #print("u0:", u0)
+    G[u0], G[u1] = G[u1], G[u0]  # ha-ha!
+    if get_pathlen(G) < len(G):
+        print("dropped!")
+
+def improving_cross(G, D, u0, u1):
+    v0, v1 = G[u0], G[u1]
+    return D[u0, u1] + D[v0, v1] < D[u0, v0] + D[u1, v1]
+
+def test_cross_swap():
+    G = {}
+    G[0] = 1
+    G[1] = 2
+    G[2] = 3
+    G[3] = 0
+    print("G0:", G)
+
+    cross_swap(G, 0, 2)
+    print("G1:", G)
 
 def greedy_init(G, D):
     node = next(iter(G))
@@ -65,7 +95,6 @@ def greedy_init(G, D):
 
 
 def tsp(points):
-    global N
     N = len(points)
     # distance matrix
     D = np.array([[np.linalg.norm(x - y) for y in points] for x in points])
@@ -78,17 +107,23 @@ def tsp(points):
         G[i] = j
 
     score = get_score(G, D)
-    for i in range(10000):
-        if i % 2000 == 0:
+    for i in range(1000000):
+        if i % 20000 == 0:
             print("{0:.1f}".format(score))
-        u = np.random.choice(N, size=3, replace=False)
-        swap(G, *u)
-        new_score = get_score(G, D)
-        if new_score <= score:
-            score = new_score
+        r = np.random.rand() < 0.5
+        if r:
+            u = np.random.choice(N, size=2, replace=False)
+            if improving_cross(G, D, *u):
+                cross_swap(G, *u)
         else:
-            #if np.random.rand() > 0: #10**((score - new_score) / 10 * np.log(i+1)):
-            swap(G, *(reversed(u)))
+            u = np.random.choice(N, size=3, replace=False)
+            triple_swap(G, *u)
+            new_score = get_score(G, D)
+            if new_score <= score:
+                score = new_score
+            else:
+                #if np.random.rand() > 0: #10**((score - new_score) / 10 * np.log(i+1)):
+                triple_swap(G, *(reversed(u)))
             
             #else:
             #    score = new_score
@@ -99,7 +134,7 @@ def tsp(points):
 def main():
     np.random.seed(0)
     #  problem parameters
-    n = 1000
+    n = 100
     k = 4
     r = 100
 
@@ -120,3 +155,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+    #test_cross_swap()
