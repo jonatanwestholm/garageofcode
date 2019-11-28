268af1f69f4c20763ea95111e5254245ba83aac7
jonatanwestholm <jonatanwestholm@gmail.com>
2019-11-28 23:44:53 +0100

    experimenting with hamiltonian

--- garageofcode/nphard/hamiltonian.py
+++ garageofcode/nphard/hamiltonian.py
@@ -25,13 +25,47 @@ def get_random_graph(n, rate, directed=False):
                     G.add_edge(i, j)
     return G
 
+def get_unsatisfied(tspath): # assumes 0/1 graph
+    path = tspath.get_path()
+    for u in path + [path[0]]:
+        if tspath.D[u, tspath.G[u]]:
+            return u
+    return None
 
-def rnr_cross(D):
-    tspath = TSPath(D=D)
+def rnr_cross(G, tspath):
+    #tspath = TSPath(D=D)
+    N = len(G)
     tspath.greedy_init()
+    score = tspath.get_score()
 
-    for i in range(1000):
-        pass
+    for i in range(10000):
+        u = get_unsatisfied(tspath)
+        if u is None:
+            return 
+
+        nodes = set(G[u]) | {u}
+        prev_G = {u: tspath.G[u] for u in tspath.G}
+        singles = list(tspath.ruin(nodes))
+        
+        # recreate step
+        tspath.recreate(singles)
+
+        tspath.exhaust_crosses()
+
+        new_score = tspath.get_score()
+        if new_score <= score:
+            score = new_score
+        else:
+            # reverse changes
+            tspath.G = prev_G
+
+        if tspath.get_pathlen() < N:
+            print("i:", i)
+            print("u:", u)
+            print("G:", tspath.G)
+            raise RuntimeError("dropped nodes!")
+        #for u in range(N):
+        #    tspath.recreate(list(tspath.ruin([u])))
 
 
 def main():
@@ -53,9 +87,12 @@ def main():
     greedy_score = tspath.get_score()
     tspath.exhaust_crosses()
     cross_score = tspath.get_score()
+    rnr_cross(G, tspath)
+    rnr_score = tspath.get_score()
 
     print("Greedy score: {0:.1f}".format(greedy_score))
     print("Cross score: {0:.1f}".format(cross_score))
+    print("RnR score: {0:.1f}".format(rnr_score))
 
 
 if __name__ == '__main__':
