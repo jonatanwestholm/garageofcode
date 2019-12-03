68affa1c7321e1a3722b3037b1ffb4518e0b2512
jonatanwestholm <jonatanwestholm@gmail.com>
2019-12-04 00:51:11 +0100

    debugging visible

--- garageofcode/highdim/main.py
+++ garageofcode/highdim/main.py
@@ -60,65 +60,71 @@ def get_corner_graph(N):
     G = nx.Graph()
     for i, u in enumerate(corners):
         u_l = list(u)
-        for i in range(N):
-            if u_l[i]:
-                u_l[i] = 0
+        for k in range(N):
+            if u_l[k]:
+                u_l[k] = 0
                 v = tuple(u_l)
                 j = corners.index(v)
                 G.add_edge(i, j)
-                u_l[i] = 1
-
+                u_l[k] = 1
     return G
 
 
-def get_closest_to_xy(P):
-    return np.argmin(np.sum(P[2:, :], axis=1))
+#def get_closest_to_xy(P):
+#    return np.argmin(np.sum(P[2:, :], axis=0))
 
 
 def get_visible(G, P, contour):
-    i = get_closest_to_xy(P)
-    P = [tuple(elem) for elem in P.T]
-    u = P[i]
-    queue = deque([i])
-    visible = {i}
+    queue = deque(contour)
+    visible = list(contour)
     while queue:
         i = queue.popleft()
         for j in G[i]:
-            if j not in visible:
-                visible.add(j)
-                if j not in contour:
-                    queue.append(j)
+            if j in visible:
+                continue
+            if np.all(P[2:, i] >= P[2:, j]):
+                visible.append(j)
+                queue.append(j)
     return visible
 
 
 def main():
     np.random.seed(0)
-    N = 3
+    N = 4
     num_iter = 100
     scale = np.sqrt(N) + 0.5
     P = ncube_corners(N).T * 2 - 1
     G = get_corner_graph(N)
     #print(points)
-    A0 = rotation(N, 5, 0.5)
+    A0 = rotation(N, 10, 0.05)
     A0 /= np.linalg.det(A0)
 
 
-    for i in range(num_iter):
+    for k in range(num_iter):
         U, contour_idxs = get_contour(P[:2, :].T)
         #print(U)
         U = list(sorted(U, key=lambda x: np.arctan2(x[1], x[0])))
         visible = get_visible(G, P, contour_idxs)
-        print(visible)
+        print(len(visible))
+        #visible = range(2**N)
+        #print(visible)
         #print(U)
         #print()
         polygon = Polygon(U, True)
         p = PatchCollection([polygon])
         fig, ax = plt.subplots()
         ax.add_collection(p)
+        for i in visible:
+            for j in G[i]:
+                if j in visible:
+                    x, y = zip(P[:2, i], P[:2, j])
+                    ax.plot(x, y, "r", zorder=99)
+        x, y = zip(*[P[:2, i] for i in visible])
+        plt.scatter(x, y, color="r", zorder=99)
         ax.set_xlim([-scale, scale])
         ax.set_ylim([-scale, scale])
         ax.axis("off")
-        plt.savefig(f"../../../results/highdim/projections/{i:04d}.png")
+        plt.savefig(f"../../../results/highdim/projections/{k:04d}.png")
         plt.close()
 
         P = np.dot(A0, P)
