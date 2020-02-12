6706e0fdfe94e4746591445dc85a7d27f1791366
jonatanwestholm <jonatanwestholm@gmail.com>
2020-02-12 22:44:51 +0100

    c quite literally 1 million times faster for euler 31

--- garageofcode/edaa25/euler3.c
+++ garageofcode/edaa25/euler3.c
@@ -14,9 +14,10 @@ int main(int argc, char* argv[]){
     clock_t t0, t1;
     t0 = clock();
 
+    // does the compiler propagate the number and do everything? answer: no.
     //long n = strtol(argv[1], NULL, 10);
-    //printf("%ld\n", n);
     long n = 600851475143;
+    //printf("%ld\n", n);
     //int n = 55;
     int p;
     while(1){
@@ -26,6 +27,6 @@ int main(int argc, char* argv[]){
     }
     
     t1 = clock();
-    printf("p: %d\n", p);
     printf("Time: %f\n", (double)(t1 - t0) / CLOCKS_PER_SEC);
+    printf("p: %d\n", p);
 }
---
+++ garageofcode/edaa25/euler31.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <time.h>
+
+int num_combs(int* vals, int target, int num_vals){
+    if(target == 0){
+        return 1;
+    }
+    if(target < 0){
+        return 0;
+    }
+    if(num_vals == 0){
+        return 0;
+    }
+
+    return num_combs(vals, target - vals[0], num_vals) +
+           num_combs(vals + 1, target, num_vals - 1);
+}
+
+int main(){
+    clock_t t0, t1;
+    t0 = clock();
+
+    int num_vals = 8;
+    int vals[8] = {200, 100, 50, 20, 10, 5, 2, 1};
+    int target = 200;
+    t1 = clock();
+    printf("Time: %f\n", (double)(t1 - t0) / CLOCKS_PER_SEC);
+
+    printf("%d\n", num_combs(vals, target, num_vals));
+
+    /*
+    int* p;
+    printf("%p\n", vals);
+    p = vals;
+    printf("%d\n", *p);
+    p = vals + 1;
+    printf("%p\n", p);
+    printf("%d\n", *p);
+    */
+}
