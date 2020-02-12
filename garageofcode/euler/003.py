06110ebdbc9538517d060fe3fd13f3c48e4dd5d1
jonatanwestholm <jonatanwestholm@gmail.com>
2020-02-12 23:28:07 +0100

    timing euler

--- garageofcode/euler/003.py
+++ garageofcode/euler/003.py
@@ -1,6 +1,8 @@
+import time
 from math import sqrt, ceil
 from eulerutils import get_primes
 
+t0 = time.time()
 n = 600851475143
 #n = 91
 largest_p = 1
@@ -14,6 +16,8 @@ while True:
             factors.append(p)
             break
     else:
+        t1 = time.time()
+        print("Time: {0:.3f}".format(t1 - t0))
         print(factors)
         print("Largest prime:", largest_p)
         break
--- garageofcode/euler/031.py
+++ garageofcode/euler/031.py
@@ -2,6 +2,7 @@
 1, 2, 5, 10, 20, 50, 100, 200
 be combined to make up the sum of 200 pence (2 pound)?
 """
+import time
 
 def num_combs(vals, target):
     if target == 0:
@@ -14,5 +15,8 @@ def num_combs(vals, target):
     return num_combs(vals, target - vals[0]) + \
            num_combs(vals[1:], target)
 
+t0 = time.time()
 values = [200, 100, 50, 20, 10, 5, 2, 1]
 print("Num ways:", num_combs(values, 200))
+t1 = time.time()
+print("Time: {0:.3f}".format(t1 - t0))
