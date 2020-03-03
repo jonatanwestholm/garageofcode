c7e3fd55d0930ca5b21ffac9bb91a4cf48ccef02
jonatanwestholm <jonatanwestholm@gmail.com>
2020-03-03 17:51:17 +0100

    +chem/equilibrium

---
+++ garageofcode/chem/equilibrium.py
@@ -0,0 +1,63 @@
+from sentian_miami import get_solver
+
+def get_concentrations(r2c, rr2k):
+    """
+    Get the concentrations of the reactant at equilibrium
+    r2c: concentrations per reactant
+    p2k: equilibrium constants per product
+
+    products are tuples ((r0, a0), (r1, a1), ...) 
+    of compounds and their correspondings cardinality, e.g
+
+    H20 = (("H", 2), ("O", 1))
+    """
+
+    solver = get_solver("couenne")
+
+    R = {r: solver.NumVar(lb=0, ub=1) for r in r2c}
+    P = {p: solver.NumVar(lb=0, ub=1) for p in p2k}
+
+    def get_card(p, r):
+        """
+        Gets the cardinality of r in p
+        Returns None if r not in p
+        """
+
+        for r_i, card in p:
+            if r_i == r:
+                return card
+        else:
+            return None
+
+
+    # mass balance equations
+    for r, rc in R.items():
+        ps = [] # parts that contain r
+        for p, pc in P.items():
+            for r_i, card in p:
+                if r_i == r:
+                    ps.append(pc * card)
+                    break
+        ps.append(rc) # the reactant itself
+        solver.Add(solver.Sum(ps) == r2c[r])
+
+    # reaction equilibrium equations
+    for p, pc in P.items():
+        rs = [] # reactant concentrations
+        for r_i, card in p:
+            if card == 1:
+                rs.append(R[r_i])
+            else:
+                rs.append(R[r_i]**card)
+        prod = p2k[p]
+        for rc in rs:
+            prod = prod * rc # don't think there is a better way
+        solver.Add(prod == pc)
+
+    solver.Solve(time_limit=10, verbose=True)
+
+    for r, rc in R.items():
+        print("{0:s}: {1:.3f}".format(r, solver.solution_value(rc)))
+
+    for p, pc in P.items():
+        print("{0:s}: {1:.3f}".format(p, solver.solution_value(pc)))
