e328a5077342884f4f6e61a69ea313137d8e2b19
jonatanwestholm <jonatanwestholm@gmail.com>
2020-03-09 20:54:04 +0100

    direct data source john hopkins

--- garageofcode/corona/corona.py
+++ garageofcode/corona/corona.py
@@ -8,10 +8,18 @@ import pandas as pd
 
 from sentian_miami import get_solver
 
-data_dir = "/home/jdw/garageofcode/data/kaggle/corona/"
+line_data_dir = "/home/jdw/garageofcode/data/kaggle/corona/"
+data_dir = "/home/jdw/repositories/COVID-19/csse_covid_19_data/csse_covid_19_time_series/"
 res_dir = "/home/jdw/garageofcode/results/corona/"
-num_cases_data = os.path.join(data_dir, "covid_19_data.csv")
-case_data = os.path.join(data_dir, "COVID19_open_line_list.csv")
+num_cases_data = os.path.join(data_dir, "time_series_19-covid-Confirmed.csv")
+case_data = os.path.join(line_data_dir, "COVID19_open_line_list.csv")
+
+latest = {"Sweden": 248, "Norway": 205, "Italy": 9172,
+            "France": 1209, "Germany": 1176, "UK": 321, 
+            "Netherlands": 321, "Belgium": 239, "Spain": 1073,
+            "Switzerland": 374, "Iran": 7161, "Japan": 511,
+            "South Korea": 7478, "Singapore": 150, 
+            "Mainland China": 80735, "US": 604}
 
 def exponential_regression(cases):
     N = len(cases)
@@ -24,18 +32,32 @@ def exponential_regression(cases):
     beta = np.exp(beta)
     return alpha, beta
 
-def case_trend():
+def get_num_confirmed(country):
     df = pd.read_csv(num_cases_data)
 
+    df = df[df["Country/Region"] == country].sum()
+    days = [datetime.strptime(date, "%m/%d/%y") for date in df.keys()[4:]]
+    last_day = max(days)
+    if country in latest:
+        val = latest[country]
+        today = last_day + timedelta(hours=24)
+        today = datetime.strftime(today, "%m/%d/%y")
+        df.at[today] = val * 1.0
+    return df.iloc[4:]
+
+def case_trend():
     for country in ["Sweden", "US", "France", "Germany", "Italy", 
                     "Iran", "UK", "South Korea", "Netherlands", 
-                    "Norway", "Belgium", "Spain", "Switzerland", "Japan"]:
-        df_country = df[df["Country/Region"] == country]
+                    "Norway", "Belgium", "Spain", "Switzerland", 
+                    "Japan", "Singapore"]:
+        #df_country = df[df["Country/Region"] == country]
         #print(df)
-        day2case = df_country.groupby(["ObservationDate"])["Confirmed"].sum()
+        #day2case = df_country.groupby(["ObservationDate"])["Confirmed"].sum()
+        day2case = get_num_confirmed(country)
         day2case = day2case[day2case >= 70]
         #print(min((day2case.keys())))
-        days = [datetime.strptime(date, "%m/%d/%Y") for date in day2case.keys()]
+        days = [datetime.strptime(date, "%m/%d/%y") for date in day2case.keys()]
+        #last_day = max(days.keys())
 
         #exit(0)
         #print(len(df_swe))
@@ -44,12 +66,18 @@ def case_trend():
         #dt = timedelta(hours=24)
         #data = np.array([2, 2, 7, 12, 14, 15, 21, 35, 94, 101, 161, 203])
         data = day2case.to_numpy()
+        data = np.array([val for val in data]) # mysteriously breaks without this
+        #if country in latest:
+        #    data = list(data)
+        #    data.append(latest[country])
+        #    data = np.array(data)
         #times = [d0 + dt * i for i in range(len(data))]
         times_arr = np.array(range(len(data)))
 
         alpha, beta = exponential_regression(data)
         #print(alpha, beta)
 
+        plt.scatter(times_arr, data)
         plt.semilogy(times_arr, data)
         plt.semilogy(times_arr, beta * alpha**times_arr, c="r")
         #plt.xticks(rotation=30)
@@ -114,15 +142,15 @@ def get_delay2freq():
 
 
 def estimate_unconfirmed():
-    df = pd.read_csv(num_cases_data)
-
-    for country in ["Mainland China", "Sweden", "US", "France", "Germany", "Italy", 
+    for country in ["Singapore", "Sweden", "US", "France", "Germany", "Italy", 
                     "Iran", "UK", "South Korea", "Netherlands", 
-                    "Norway", "Belgium", "Spain", "Switzerland", "Japan"]:
-        df_country = df[df["Country/Region"] == country]
-        day2case = df_country.groupby(["ObservationDate"])["Confirmed"].sum()
+                    "Norway", "Belgium", "Spain", "Switzerland", 
+                    "Japan", "Mainland China"]:
+        #df_country = df[df["Country/Region"] == country]
+        #day2case = df_country.groupby(["ObservationDate"])["Confirmed"].sum()
+        day2case = get_num_confirmed(country)
         #day2case = day2case[day2case >= 70]
-        days = [datetime.strptime(date, "%m/%d/%Y") for date in day2case.keys()]
+        days = [datetime.strptime(date, "%m/%d/%y") for date in day2case.keys()]
 
         total_confirmed = day2case.to_numpy()
         times_arr = np.array(range(len(total_confirmed)))
@@ -165,10 +193,17 @@ def estimate_unconfirmed():
         for tn_conf, tn_case in zip(total_confirmed, total_cases):
             solver.Add(tn_case >= tn_conf)
 
+        #mass: baseline hypothesis is that number of new cases is 0
+        #mass = solver.Sum(nc for nc in new_cases)        
+        mass_sq = solver.Sum(nc**2 for nc in new_cases)
+        
+        #diff: baseline hypothesis is that number of new cases is like yesterday
         #diff = solver.Sum([(nc1 - nc0)**2 for nc0, nc1 in zip(new_cases, new_cases[1:])])
-        mass = solver.Sum(nc**2 for nc in new_cases)
+        
+        #exp_diff: baseline hypothesis is that number of new cases grow by 30% each day
+        #exp_diff = solver.Sum([(nc1 - 1.3 * nc0)**2 for nc0, nc1 in zip(new_cases, new_cases[1:])])
 
-        solver.SetObjective(total_err + mass * 0.1, maximize=False)
+        solver.SetObjective(total_err + mass_sq * 0.1, maximize=False)
         solver.Solve(time_limit=10, verbose=False)
 
         new_cases_solve = [solver.solution_value(nc) for nc in new_cases]
@@ -176,7 +211,7 @@ def estimate_unconfirmed():
 
         plt.plot(range(cutoff_days - 1, N + cutoff_days), total_confirmed, c="b")
         plt.plot(range(N + cutoff_days), total_cases, c="r")
-        plt.title("Estimated unconfirmed {}".format(country))
+        plt.title("Estimated best case unconfirmed {}".format(country))
         plt.xlabel("Days")
         plt.legend(["Confirmed cases", "Estimated total incl. unconfirmed"])
         plt.savefig(os.path.join(res_dir, "est_unconfirmed/unconfirmed_{}.png".format(country)))
@@ -185,3 +220,4 @@ def estimate_unconfirmed():
 
 if __name__ == '__main__':
     estimate_unconfirmed()
+    #case_trend()
