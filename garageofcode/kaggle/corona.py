9daef95d830944b357e9617d689ac14756ea45dd
jonatanwestholm <jonatanwestholm@gmail.com>
2020-03-09 09:42:14 +0100

    corona delay from onset to confirmation

--- garageofcode/kaggle/corona.py
+++ garageofcode/kaggle/corona.py
@@ -17,8 +17,7 @@ def exponential_regression(cases):
     beta = np.exp(beta)
     return alpha, beta
 
-
-def main():
+def case_trend():
     fn_dir = "/home/jdw/garageofcode/data/kaggle/corona/"
     fn = os.path.join(fn_dir, "covid_19_data.csv")
 
@@ -56,9 +55,67 @@ def main():
         plt.legend(["data", "{0:.2f} * {1:.2f}^t".format(beta, alpha)])
         #plt.show()
 
-        plt.savefig("/home/jdw/garageofcode/results/kaggle/corona/{}.png".format(country))
+        plt.savefig("/home/jdw/garageofcode/results/corona/{}.png".format(country))
         plt.close()
 
 
+def get_do_dc(df):
+    dos = []
+    dcs = []
+
+    num_errors = 0
+
+    for do, dc in zip(df["date_onset_symptoms"], df["date_confirmation"]):
+        try:
+            do = datetime.strptime(do, "%d.%m.%Y")
+            dc = datetime.strptime(dc, "%d.%m.%Y")
+        except TypeError as e:
+            print("do:", do)
+            print("dc:", dc)
+            raise e
+        except ValueError as e:
+            #print("ValueError")
+            #print("do:", do)
+            #print("dc:", dc)
+            #print()
+            num_errors += 1
+            continue
+
+        dos.append(do)
+        dcs.append(dc)
+
+    print("Number of row errors:", num_errors)
+
+    return dos, dcs
+
+
+def delay():
+    fn_dir = "/home/jdw/garageofcode/data/kaggle/corona/"
+    fn = os.path.join(fn_dir, "COVID19_open_line_list.csv")
+
+    df = pd.read_csv(fn)
+
+    df = df.dropna(subset=["date_onset_symptoms", "date_confirmation"])
+
+    country2ncases = df.groupby("country").size()
+    print(country2ncases)
+    exit(0)
+
+    date_onsets, date_conf = get_do_dc(df)
+
+    #date_onsets = [datetime.strptime(date, "%d.%m.%Y") for date in df["date_onset_symptoms"]]
+    #date_conf   = [datetime.strptime(date, "%d.%m.%Y") for date in df["date_confirmation"]]
+
+    diff = np.array([(dc - do).total_seconds() / (3600 * 24) 
+                        for do, dc in zip(date_onsets, date_conf) if dc >= do])
+
+    plt.hist(-diff, bins=2*int(max(diff)))
+    plt.title("N={}".format(len(diff)))
+    plt.xlabel("Days from onset to confirmation")
+    plt.ylabel("Cases")
+    plt.show()
+
+
+
 if __name__ == '__main__':
-    main()
+    delay()
