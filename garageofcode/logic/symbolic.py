12837783aecd99db9daaf2bf96ad5e9557b0c738
jonatanwestholm <jonatanwestholm@gmail.com>
2020-06-13 23:59:20 +0200

    working on symbolic

--- garageofcode/logic/symbolic.py
+++ garageofcode/logic/symbolic.py
@@ -1,3 +1,39 @@
+
+def get_subexpr(s):
+    d = 1
+    for i, ch in enumerate(s):
+        if ch == "(":
+            d += 1
+        elif ch == ")":
+            d -= 1
+        if not d:
+            break
+    else:
+        raise # Unmatched left parenthesis
+    return s[:i]
+
+
+def get_token(s):
+    s = s.strip()
+    
+
+
+def tokenize(s):
+    while s:
+        tok, s = get_token(s)
+        if tok == "(":
+            sub_s, s = get_subexpr(s)
+            yield parse(s)
+        else:
+            yield tok
+
+
+def parse(s):
+    tokens = list(tokenize(s))
+    # magic happens, returns an expression
+
+
+
 class Expression:
     def __init__(self, op, lhs, rhs=None):
         self.op = op
@@ -8,11 +44,76 @@ class Expression:
         if self.rhs is None:
             return str(self.op) + "(" + str(self.lhs) + ")"
         else:
-            return str(self.lhs) + self.op + str(self.rhs)
+            return "(" + str(self.lhs) + self.op + str(self.rhs) + ")"
+
+    def simplify(self):
+        try:
+            self.lhs = self.lhs.simplify()
+        except AttributeError:
+            pass
+
+        try:
+            self.rhs = self.rhs.simplify()
+        except AttributeError:
+            pass
+
+        if self.op == "+":
+            try:
+                return self.lhs + self.rhs / 1
+            except TypeError:
+                pass
+
+            if self.lhs == 0:
+                return self.rhs
+            if self.rhs == 0:
+                return self.lhs
+            return self
+
+        if self.op == "-":
+            try:
+                return self.lhs - self.rhs
+            except TypeError:
+                pass
+
+            if self.lhs == 0:
+                return Expression("*", -1, self.rhs).simplify()
+            if self.rhs == 0:
+                return self.lhs
+            return self
+
+        if self.op == "*":
+            try:
+                return self.lhs * self.rhs / 1
+            except TypeError:
+                pass
+
+            if self.rhs == 0:
+                return 0
+            if self.lhs == 0:
+                return 0
+            if self.lhs == 1:
+                return self.rhs
+            if self.rhs == 1:
+                return self.lhs
+            return self
+
+        if self.op == "/":
+            try:
+                return self.lhs / self.rhs
+            except TypeError:
+                pass
+
+            if self.lhs == 0:
+                return 0
+            if self.lhs == 1:
+                return self
+            if self.rhs == 1:
+                return self.lhs
+            return self
 
 
 def differentiate(expr, x):
-    print(expr)
+    #print(expr)
     try:
         expr.op
     except AttributeError:
@@ -56,8 +157,9 @@ def differentiate(expr, x):
                           )
 
 def main():
-    expr = Expression("/", 2, "x")
-    print(differentiate(expr, "x"))
+    expr = Expression("*", 2, "x")
+    print(expr.simplify())
+    #print(differentiate(expr, "x").simplify())
 
 
 if __name__ == '__main__':
