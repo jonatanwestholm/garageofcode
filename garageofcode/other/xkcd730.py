3cfaf01f6d77130bb354887e6ed9921c791db849
Jonatan Westholm <jonatanwestholm@gmail.com>
2021-11-08 15:38:32 +0100

    upgrade to babelsberg

--- garageofcode/other/xkcd730.py
+++ garageofcode/other/xkcd730.py
@@ -4,7 +4,7 @@ import numpy as np
 import networkx as nx
 
 #from garageofcode.mip.solver import get_solver
-from sentian_miami import get_solver
+from babelsberg import get_solver
 
 def get_xkcd730_graph():
     G = nx.DiGraph()
@@ -40,28 +40,28 @@ def get_potentials(G, s, t):
     # it's just a linear equation system.
     # But it makes for a simple formulation.
 
-    solver = get_solver("mono")
+    solver = get_solver("couenne")
 
     t0 = time.time()
-    potentials = {node: solver.NumVar(lb=0) for node in G}
-    currents = {e: solver.NumVar() for e in G.edges}
+    potentials = {node: solver.num_var(lb=0) for node in G}
+    currents = {e: solver.num_var() for e in G.edges}
 
     # Edge conditions
     U0, U_1 = 1, 0
     total_potential = U0 - U_1
-    solver.Add(potentials[s] == U0)
-    solver.Add(potentials[t] == U_1)
+    solver.add(potentials[s] == U0)
+    solver.add(potentials[t] == U_1)
 
     # Kirchoff's law: current is preserved in internal nodes
     for node in G:
-        in_current = solver.Sum([currents[e] for e in G.in_edges(node)])
-        out_current = solver.Sum([currents[e] for e in G.out_edges(node)])
+        in_current = solver.sum([currents[e] for e in G.in_edges(node)])
+        out_current = solver.sum([currents[e] for e in G.out_edges(node)])
         if node == s:
             total_in = out_current - in_current
         elif node == t:
             total_out = in_current - out_current
         else:
-            solver.Add(in_current == out_current)
+            solver.add(in_current == out_current)
 
     # Ohm's law: delta U = I * R
     for e in G.edges:
@@ -70,12 +70,12 @@ def get_potentials(G, s, t):
         Uj = potentials[j]
         Iij = currents[e]
         Rij = 1 # ignore resistance parameter for now
-        solver.Add(Ui - Uj == Rij * Iij)
+        solver.add(Ui - Uj == Rij * Iij)
 
     t1 = time.time()
     print("Build time: {0:.3f}".format(t1 - t0))
 
-    solver.Solve(time_limit=10, verbose=True)
+    solver.solve(time_limit=10, verbose=True)
 
     total_current = solver.solution_value(total_in)
     total_resistance = total_potential / total_current
